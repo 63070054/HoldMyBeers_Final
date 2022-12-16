cbcf9223207e0c86dbf6a202e3129fc520b89156
ntp_vv <63070054@kmitl.ac.th>
2022-12-16 18:31:26 +0700

    Run Server

--- src/main/java/command/rest/BeerCommandController.java
+++ src/main/java/command/rest/BeerCommandController.java
@@ -1,18 +1,15 @@
 package command.rest;
 
-import org.springframework.web.bind.annotation.*;
 import service.BeerService;
 
-@RestController
-@RequestMapping("/beers")
-public class BeerCommandController {
-//    private final BeerService beerService;
-//
-//    public BeerCommandController(BeerService beerService) {
-//        this.beerService = beerService;
-//    }
 
-    @PostMapping
+public class BeerCommandController {
+    private final BeerService beerService;
+
+    public BeerCommandController(BeerService beerService) {
+        this.beerService = beerService;
+    }
+
     public String createBeer(){
         return "sussec";
     }
