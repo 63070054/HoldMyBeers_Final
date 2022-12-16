fa555e59320983dfe7bf2fd1802c9bf8228f87c7
KITTIPOB PANGTRAKOON <63070010@kmitl.ac.th>
2022-12-16 18:19:36 +0700

    ของแบงค์

--- src/main/java/command/rest/BeerCommandController.java
+++ src/main/java/command/rest/BeerCommandController.java
@@ -1,14 +1,23 @@
 package command.rest;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import service.BeerService;
 
+@RestController
+@RequestMapping("/Beer")
 public class BeerCommandController {
     private final BeerService beerService;
 
     public BeerCommandController(BeerService beerService) {
         this.beerService = beerService;
     }
-
+    @PostMapping
+    public String createBeer(@RequestBody CreateBeerRestModel model){
+        return "sussec" + model.getBeerName();
+    }
 
 
 }
