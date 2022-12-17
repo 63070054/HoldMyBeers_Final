9035d7d279634445d754fd983d00109b53b51758
ntp_vv <63070054@kmitl.ac.th>
2022-12-17 11:06:58 +0700

    Add More Override Method in Proto

--- src/main/java/holdMyBeer/service/BeerService.java
+++ src/main/java/holdMyBeer/service/BeerService.java
@@ -10,17 +10,37 @@ public class BeerService extends BeerServiceGrpc.BeerServiceImplBase {
     }
 
     @Override
+    public void signInDecomposition(SignInRequest request, StreamObserver<SignInResponse> responseObserver) {
+        super.signInDecomposition(request, responseObserver);
+    }
+
+    @Override
+    public void queryBeersDecomposition(QueryBeersRequest request, StreamObserver<QueryBeerByIdResponse> responseObserver) {
+        super.queryBeersDecomposition(request, responseObserver);
+    }
+
+    @Override
+    public void addBeerToFavorite(AddBeerToFavoriteRequest request, StreamObserver<AddBeerToFavoriteResponse> responseObserver) {
+        super.addBeerToFavorite(request, responseObserver);
+    }
+
+    @Override
+    public void removeBeerToFavorite(RemoveBeerToFavoriteRequest request, StreamObserver<RemoveBeerToFavoriteResponse> responseObserver) {
+        super.removeBeerToFavorite(request, responseObserver);
+    }
+
+    @Override
     public void queryBeerByIdDecomposition(QueryBeerByIdRequest request, StreamObserver<QueryBeerByIdResponse> responseObserver) {
 
     }
 
     @Override
     public void editBeerDecomposition(EditBeerRequest request, StreamObserver<EditBeerResponse> responseObserver) {
-        super.editBeerDecomposition(request, responseObserver);
+
     }
 
     @Override
     public void deleteBeerDecomposition(DeleteBeerRequest request, StreamObserver<DeleteBeerResponse> responseObserver) {
-        super.deleteBeerDecomposition(request, responseObserver);
+
     }
 }
