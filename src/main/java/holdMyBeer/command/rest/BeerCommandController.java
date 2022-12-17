db60622109d06f6a3ea1ce7326df7dcb6fb23ecc
Sorachat <73382296+ter2002@users.noreply.github.com>
2022-12-17 16:40:51 +0700

    RestAPI CRUD Beer in Command And Query

--- src/main/java/holdMyBeer/command/rest/BeerCommandController.java
+++ src/main/java/holdMyBeer/command/rest/BeerCommandController.java
@@ -1,15 +1,33 @@
 package holdMyBeer.command.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import holdMyBeer.query.rest.BeerRestModel;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/beer")
+@RequestMapping("/beers")
 public class BeerCommandController {
+    @Autowired
+    private BeerService beerService;
 
+    // Create Beer
+    @PostMapping
+    public BeerRestModel createBeer(@RequestBody BeerRestModel beerRestModel){
+//        BeerRestModel beerRestModel = new BeerRestModel();
+//        beerRestModel.setBeerName(request.getBeerName());
+//        return beerRestModel;
+        return beerService.createBeer(beerRestModel)
+    }
 
-    public String createBeer(){
-        return "sussec";
+    // Update Beer
+    @PutMapping("/{id}")
+    public BeerRestModel updateBeer(@PathVariable Long id, @RequestBody BeerRestModel beerRestModel) {
+        return beerService.updateBeer(id, beerRestModel);
+    }
+
+    // Delete Beer
+    @DeleteMapping("/{id}")
+    public void deleteBeer(@PathVariable Long id) {
+        beerService.deleteQuery(id);
     }
 
 
--- src/main/java/holdMyBeer/query/rest/BeerQueryController.java
+++ src/main/java/holdMyBeer/query/rest/BeerQueryController.java
@@ -1,30 +1,24 @@
 package holdMyBeer.query.rest;
 
-
-import holdMyBeer.query.BeerQueryHandler;
-import holdMyBeer.query.FindBeerQuery;
-import org.axonframework.messaging.responsetypes.ResponseType;
-import org.axonframework.messaging.responsetypes.ResponseTypes;
-import org.axonframework.queryhandling.QueryGateway;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/beers")
 public class BeerQueryController {
-
     @Autowired
-    QueryGateway queryGateway;
-    @GetMapping
-    public List<BeerRestModel> getbeer(){
-        FindBeerQuery findBeerQuery = new FindBeerQuery();
-        List<BeerRestModel> beers = queryGateway.query(findBeerQuery, ResponseTypes.multipleInstancesOf(BeerRestModel.class)).join();
-        return  beers;
+    private BeerService beerService;
+
+    // Read Beer By id
+    @GetMapping("/{id}")
+    public BeerRestModel getBeer(@PathVariable Long id) {
+        return beerService.getBeerById(id);
+    }
+
+    // Read Beer By name
+    @GetMapping("/{name}")
+    public List<Beer> getBeer(@PathVariable String  name) {
+        return beerService.getBeerByName(name);
     }
 
 
